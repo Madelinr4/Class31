c0ece338c73288b500ca52386e20b246faa4d200
Madeline <kittykatbarber2006@gmail.com>
2021-02-04 19:10:30 -0500

    added trajectory

--- sketch.js
+++ sketch.js
@@ -1,3 +1,56 @@
+//examples on the different types of data in js
+
+//string
+var string = "maggie";
+console.log(string);
+
+//number
+var number = 8;
+console.log(number);
+
+//boolean
+var boolean = true;
+console.log(boolean);
+
+//undefined
+var undefined;
+console.log(undefined);
+
+//reassigning the same undefined object with null
+undefined = null;
+console.log(undefined);
+
+//an array holding same data type
+var array1 = [1, 2, 3, 4];
+console.log(array1);
+
+//array holding different data types
+var array2 = ["noodle", false, 24];
+console.log(array2);
+
+//array storing a list of arrays
+var array3 = [["spicy", 7],[true, null],[90, "jack", false]];
+console.log(array3);
+
+//accessing the second element of array2
+console.log(array2[1]);
+
+//accessing the second element of the third element of array3
+console.log(array3[2][1]);
+
+//checking the length of an array
+console.log(array2.length);
+
+//adding an element in an existing array
+array3.push(90);
+console.log(array3);
+
+//removing an element from an array
+array1.pop(1);
+console.log(array1);
+
+
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -7,7 +60,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var gameState = "onsling";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,16 +122,19 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState !== "launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched";
 }
 
-function keyPressed(){
-    if(keyCode === 32){
-        slingshot.attach(bird.body);
-    }
-}
+//function keyPressed(){
+    //if(keyCode === 32){
+        //slingshot.attach(bird.body);
+    //}
+//}
